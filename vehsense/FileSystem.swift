e0e481a3f919cee1893a62f93b0b26fbe1b03567
Jalil Sarwari <jalilsar@buffalo.edu>
2018-06-04 16:27:46 -0400

    Signed-off-by: Jalil Sarwari <jalilsar@buffalo.edu>

---
+++ vehsense/FileSystem.swift
@@ -0,0 +1,16 @@
+//
+//  FileSystem.swift
+//  vehsense
+//
+//  Created by Jalil Sarwari on 6/4/18.
+//  Copyright © 2018 Weida Zhong. All rights reserved.
+//
+
+import Foundation
+
+class FileSystem{
+    
+    func FileSystem(){
+        
+    }
+}
---
+++ vehsense/GPS.swift
@@ -0,0 +1,25 @@
+//
+//  GPS.swift
+//  vehsense
+//
+//  Created by Jalil Sarwari on 6/4/18.
+//  Copyright © 2018 Weida Zhong. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+class GPS: FileSystem{
+    var folderDesti = ""
+    
+    func GPS(_ folderDest: String){
+        folderDesti = folderDest
+    }
+    func Go(){
+        //This is what should happen when the go button is clicked
+        
+    }
+    func Stop(){
+        
+    }
+}
